823881836960218c21aa6e17d9f6063dfc2b870e
Marie Dillard and Mark Strickland <mark@>
2019-01-17 13:44:30 -0800

    Final Commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $("form#survey").submit(function(event) {
+  $("form#dimensions").submit(function(event) {
     event.preventDefault();
-    var age = $("input:radio[name=age]:checked").val();
-    if (age === "over18") {
-      $("#survey").hide();
-      $("#over18").show();
-    } else {
-      $("#survey").hide();
-      $("#under18").show();
+    var response = "isoceles";
+    var side1 = parseFloat($("#side1").val());
+    var side2 = parseFloat($("#side2").val());
+    var side3 = parseFloat($("#side3").val());
+    //debugger;
+    if ( isNaN(side1) || isNaN(side2) ||  isNaN(side3) ) {
+      response = "One or more values are not numbers, please enter a number!";
+    } else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+      response = "not a triangle";
+    } else if (side1 != side2 && side2 != side3 && side1 != side3) {
+      response = "scalene";
+    } else if (side1 === side2 && side2 === side3 && side3 === side1) {
+      response = "equilateral";
     }
+    $("#output").empty().append(response);
 	});
 });
